ee78d42d9822bb20d22477fe71a16c8671cedaee
danleao <daniel.leao@zoppy.com.br>
2023-06-02 16:30:59 -0300

    Fix formatTemplateBody function

--- src/util/whatsapp-utilities.spec.ts
+++ src/util/whatsapp-utilities.spec.ts
@@ -20,8 +20,26 @@ describe(`Whatsapp Utilities`, () => {
         const body: string =
             'Oi {{1}} ,\\n\n\nQueremos te contar em primeira mão o super presente de Dia dos namorados que preparamos!\\n\n\nPara deixar a experiência de autocuidado ainda mais completa e gostosa, nos unimos ao Namah Beauty 🤩!!\n\n\nE durante o mês de junho (ou enquanto durar os estoques), todas as compras no nosso site receberão de presente o shampoo e o condicionador sólidos do Namah!\n\n\nSua pele e seus cabelos agradecem!\n\n\nAproveite para garantir o seu e presentear quem você ama! 🤎\n';
         const result: string = WhatsappUtilities.formatTemplateBody(body);
+        console.log(result);
         expect(result).toEqual(
-            'Oi {{1}} ,\n \n \n Queremos te contar em primeira mão o super presente de Dia dos namorados que preparamos!\n \n \n Para deixar a experiência de autocuidado ainda mais completa e gostosa, nos unimos ao Namah Beauty 🤩!!\n \n \n E durante o mês de junho (ou enquanto durar os estoques), todas as compras no nosso site receberão de presente o shampoo e o condicionador sólidos do Namah!\n \n \n Sua pele e seus cabelos agradecem!\n \n \n Aproveite para garantir o seu e presentear quem você ama! 🤎\n '
+            `
+Oi {{1}} ,
+
+
+Queremos te contar em primeira mão o super presente de Dia dos namorados que preparamos!
+
+
+Para deixar a experiência de autocuidado ainda mais completa e gostosa, nos unimos ao Namah Beauty 🤩!!
+
+
+E durante o mês de junho (ou enquanto durar os estoques), todas as compras no nosso site receberão de presente o shampoo e o condicionador sólidos do Namah!
+
+
+Sua pele e seus cabelos agradecem!
+
+
+Aproveite para garantir o seu e presentear quem você ama! 🤎
+ `
         );
     });
 });
