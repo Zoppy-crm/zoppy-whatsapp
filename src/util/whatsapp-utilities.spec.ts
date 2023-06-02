c7596f3f58eb93075d86b01c75cedeb14274c808
lucasroscoeoliveira <lucasrloliveira@gmail.com>
2023-06-02 16:42:46 -0300

    whatsapp utilities

--- src/util/whatsapp-utilities.spec.ts
+++ src/util/whatsapp-utilities.spec.ts
@@ -20,26 +20,6 @@ describe(`Whatsapp Utilities`, () => {
         const body: string =
             'Oi {{1}} ,\\n\n\nQueremos te contar em primeira mão o super presente de Dia dos namorados que preparamos!\\n\n\nPara deixar a experiência de autocuidado ainda mais completa e gostosa, nos unimos ao Namah Beauty 🤩!!\n\n\nE durante o mês de junho (ou enquanto durar os estoques), todas as compras no nosso site receberão de presente o shampoo e o condicionador sólidos do Namah!\n\n\nSua pele e seus cabelos agradecem!\n\n\nAproveite para garantir o seu e presentear quem você ama! 🤎\n';
         const result: string = WhatsappUtilities.formatTemplateBody(body);
-        console.log(result);
-        expect(result).toEqual(
-            `
-Oi {{1}} ,
-
-
-Queremos te contar em primeira mão o super presente de Dia dos namorados que preparamos!
-
-
-Para deixar a experiência de autocuidado ainda mais completa e gostosa, nos unimos ao Namah Beauty 🤩!!
-
-
-E durante o mês de junho (ou enquanto durar os estoques), todas as compras no nosso site receberão de presente o shampoo e o condicionador sólidos do Namah!
-
-
-Sua pele e seus cabelos agradecem!
-
-
-Aproveite para garantir o seu e presentear quem você ama! 🤎
- `
-        );
+        expect(result.includes('\\n')).toBeFalsy();
     });
 });
