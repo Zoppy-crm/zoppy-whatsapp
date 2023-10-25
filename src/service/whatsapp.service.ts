b5fe97ff5745b19794657d39d2d0d74fe1083ccb
lucasroscoezoppy <lucas.roscoe@zoppy.com.br>
2023-10-25 14:47:33 -0300

    adjust get conversations wpp service

--- src/service/whatsapp.service.ts
+++ src/service/whatsapp.service.ts
@@ -5,12 +5,15 @@ import { WhatsappConversationResponse } from '../response/whatsapp-conversation.
 
 export class WhatsappService {
     public static async getConversations(request: WhatsappConversationRequest): Promise<WhatsappConversationResponse> {
-        const url: string = WhatsappRoutes.makeBaseUrl();
+        const url: string = WhatsappRoutes.getWhatsappBusinessAccountUrl(request.wabaId);
 
         const params: any = {
             fields: `conversation_analytics.start(${request.from}).end(${request.to}).granularity(DAILY).phone_numbers([]).dimensions(["conversation_type", "conversation_direction", "phone"])`,
             access_token: `${request.apiAccessToken}`
         };
+
+        console.log(JSON.stringify(params));
+        console.log(JSON.stringify(url));
 
         const response: any = axios.get(url, { params: params });
         return response.data;
